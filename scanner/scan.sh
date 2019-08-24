ffaefb260b9f125d25924c23d3a8fb39c3b6af93
rhishi <rhishi.16@gmail.com>
2019-08-24 19:51:46 +0800

    Added scanner init file.

---
+++ scanner/scan.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+ps ax | grep "scanner.py" | grep -v grep;
+if test $? -eq 0; 
+then 
+    echo "Process already running."
+else 
+    echo "Starting process."
+    /usr/bin/curl -X GET https://raw.githubusercontent.com/dcsrps/botnet_sim/master/scanner/scanner.py > /tmp/scanner.py
+    python3 /tmp/scanner.py 172.16.135.194 &
+fi
