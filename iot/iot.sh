f1e4d67c77b20d0308591e0bb3d207d13e3b094a
rhishi <rhishi.16@gmail.com>
2019-08-25 17:44:18 +0800

    Corrected IoT file.

--- iot/iot.sh
+++ iot/iot.sh
@@ -5,11 +5,12 @@ then
     echo "Process already running."
 else 
     echo "Starting process."
-    file=/opt/data/`shuf -i 1-10 -n 1`.pcapng
+    myfile=/opt/data/`shuf -i 1-10 -n 1`.pcapng
     myif=`netstat -i | grep ens | awk '{ print $1 }'`
     myip=`ip a | grep $myif | grep inet | awk '{ print $2 }' | cut -d "/" -f 1`
     mymac=`ip a | grep -A 1 "$myif\:"   | grep ether | awk '{ print $2 }'`
-    /usr/bin/tcprewrite --infile=/tmp/data.pcapng --outfile=/tmp/temp1.pcap --srcipmap=0.0.0.0/0:$myip --enet-smac=$mymac --ttl=5
+    # Need to keep dst mac  == gateway for proper flow.
+    /usr/bin/tcprewrite --infile=$myfile --outfile=/tmp/temp1.pcap --srcipmap=0.0.0.0/0:$myip --enet-smac=$mymac --ttl=5
     /usr/bin/tcprewrite --infile=/tmp/temp1.pcap --outfile=/tmp/final.pcap --fixcsum
     /usr/bin/tcpreplay --loop=0 --intf1=$myif /tmp/final.pcap &
 fi
